7176011f1822a522da7e4e134c29b14c8b9b5a9f
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-10-05 00:47:20 +0800

    Add files via upload

---
+++ asm2/RegularPolygon.java
@@ -0,0 +1,150 @@
+/** 
+* This class is used to model regular n-sided polygons. It is a subclass of Shape.
+* 
+* @author Li Hoi Kit
+*/
+public class RegularPolygon extends Shape{
+    //Variables
+
+    private int numOfSides; //Number of sides of the polygon
+    private double radius; //The radius of the polygon
+
+    //Constructors
+
+    /** 
+     * Builds a n-sided polygon with radius r
+     * @param n number of sides (sets to 3 if this argument is less than 3)
+     * @param r radius (sets to 0.0 if this argument is less than 0.0)
+    */
+    public RegularPolygon(int n, double r){
+        this.radius = r;
+        this.numOfSides = n;
+        this.setVertices();
+    }
+
+    /** 
+     * Builds a n-sided polygon with radius 1.0
+     * @param n number of sides (sets to 3 if this argument is less than 3)
+    */
+    public RegularPolygon(int n){
+        this.radius = 1.0;
+        this.numOfSides = n;
+        this.setVertices();
+    }
+
+    /** 
+     * Builds a 3-sided polygon with radius 1.0
+    */
+    public RegularPolygon(){
+        this.radius = 1.0;
+        this.numOfSides = 3;
+        this.setVertices();
+    }
+
+    //Methods
+    
+    /** 
+     * Getter method for retrieving the number of sides of the polygon
+     * @return int Number of sides of the polygon
+     */
+
+    public int getNumOfSides(){
+        return this.numOfSides;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the radius of the polygon
+     * @return double Radius of the polygon
+     */
+    public double getRadius(){
+        return this.radius;
+    }
+
+    
+    /** 
+     * Setter method for setting the number of sides of the polygon
+     * @param n number of sides (sets to 3 if this argument is less than 3)
+     */
+    public void setNumOfSides(int n){
+        if(n < 3)
+            this.numOfSides = 3;
+        else
+            this.numOfSides = n;
+
+        this.setVertices(); //Reset the local coordinates
+    }
+
+    
+    /** 
+     * Setter method for setting the radius of the polygon
+     * @param r radius of the polygon
+     */
+    public void setRadius(double r){
+        if(r < 0.0)
+            this.radius = 0.0;
+        else
+            this.radius = r;
+
+        this.setVertices(); //Reset the local coordinates
+    }
+
+    private void setVertices(){
+        //Allocate memory in heap for the coordinates arrays
+        double[] xLocal = new double[this.numOfSides];
+        double[] yLocal = new double[this.numOfSides];
+
+        //Determine value of a
+        double a;
+        if(this.numOfSides % 2 == 0) //Number of sides is even
+            a = Math.PI/this.numOfSides;
+        else //Number of sides is odd
+            a = 0;
+
+        //Calculates the coordinates of the vertex and assign them to the arrays
+        for(int i = 0; i < this.numOfSides; i++){
+            xLocal[i] = this.radius * Math.cos(a - (i * (2 * Math.PI)) / this.numOfSides );
+            yLocal[i] = this.radius * Math.sin(a - (i * (2 * Math.PI)) / this.numOfSides );
+        }
+
+        //Use the setters to assign the vertices to the private xLocal yLocal references inherited from the superclass
+        this.setXLocal(xLocal);
+        this.setYLocal(yLocal);
+    } 
+
+    
+    /** 
+     * Determines whether a point in screen coordinate system is contained (i.e. lies inside or on sides/vertices) by the polygon.
+     * @param x x-coordinate of the point in screen coordinate system
+     * @param y y-coordinate of the point in screen coordinate system
+     * @return boolean the truth value of whether the point is in the polygon
+     */
+    public boolean contains(double x, double y){
+
+        //Retrieve the required information from the getter methods inherited from the super class
+        double pointX = (x - this.getXc()) * Math.cos(-this.getTheta()) - (y - this.getYc()) * Math.sin(-this.getTheta());
+        double pointY = (x - this.getXc()) * Math.sin(-this.getTheta()) + (y - this.getYc()) * Math.cos(-this.getTheta());
+        double[] xLocal = this.getXLocal();
+
+        //The vertex with minimum value of X is at the exactly opposite side of the first vertex for even number of sides(which is the n/2th vertex), or at the (n-1)/2th vertex for odd number of sides (-1 is omitted as Java automatically take away decimal places for integer division)
+        double minX = xLocal[this.getNumOfSides()/2];
+        
+        //Compare the x coordinate of the point with the x coordinate of the left most side of the polygon(i.e. with minimum x-value). Return false if point is at the left of the side 
+        if(minX > pointX)
+            return false;
+            
+        for(int i = 0; i < this.numOfSides - 1; i++){
+            //Rotate
+            double originalX = pointX;
+            double originalY = pointY;
+            pointX = originalX * Math.cos((2 * Math.PI)/this.numOfSides) - originalY * Math.sin((2 * Math.PI)/this.numOfSides);
+            pointY = originalX * Math.sin((2 * Math.PI)/this.numOfSides) + originalY * Math.cos((2 * Math.PI)/this.numOfSides);
+
+            //Check
+            if(minX > pointX)
+            return false;
+        }
+
+        return true; //Point isnt outside of the shape, return true
+    }
+}
---
+++ asm2/Shape.java
@@ -0,0 +1,194 @@
+import java.awt.Color;
+import java.lang.Math;
+
+/** 
+* This class is used to model general shapes. This class should be inherited by other subclasses to be functional.
+* 
+* @author Li Hoi Kit
+*/
+public class Shape {
+    //Private variables
+    
+    private Color color; //Color of the shape
+    private boolean filled; //Fill-type of the shape
+    private double theta; //orientation of the shape (in radians)
+    private double xc; //x coordinate of center
+    private double yc; //y coordinate of center
+    private double[] xLocal; //x coordinates of the vertices
+    private double[] yLocal; //y coordinates of the vertices
+
+    
+
+    //Getter methods
+
+    /** 
+     * Getter method for retrieving the color of the shape
+     * @return Color the color
+     */
+    public Color getColor(){
+        return this.color;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the fill-type of the shape
+     * @return boolean the fill-type
+     */
+    public boolean getFilled(){
+        return this.filled;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the orientation (in radians) of the shape in screen coordinate system
+     * @return double orientation (in radians)
+     */
+    public double getTheta(){
+        return this.theta;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the x-coordinate of the center of the shape in the screen coordinate system
+     * @return double the x-coordinate of the center
+     */
+    public double getXc(){
+        return this.xc;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the y-coordinate of the center of the shape in the screen coordinate system
+     * @return double the y-coordinate of the center
+     */
+    public double getYc(){
+        return this.yc;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the x-coordinates of the vertices (in counter-clockwise order) of the shape in its local coordinate system
+     * @return double[] an array that contains the x-coordinates of the vertices
+     */
+    public double[] getXLocal(){
+        return this.xLocal;
+    }
+
+    
+    /** 
+     * Getter method for retrieving the y-coordinates of the vertices (in counter-clockwise order) of the shape in its local coordinate system
+     * @return double[] an array that contains the y-coordinates of the vertices
+     */
+    public double[] getYLocal(){
+        return this.yLocal;
+    }
+
+    //Setter Methods
+
+    /** 
+     * Setter method for setting the color of the shape
+     * @param color The color
+     */
+
+    public void setColor(Color color){
+        this.color = color;
+    }
+
+    
+    /** 
+     * Setter method for setting the fill-type of the shape
+     * @param filled The fill-type
+     */
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+
+    
+    /** 
+     * Setter method for setting the orientation of the shape (in radians)
+     * @param theta the orientation (in radians)
+     */
+    public void setTheta(double theta){
+        this.theta = theta;
+    }
+
+    
+    /** 
+     * Setter method for setting the x-coordinate of the center of the shape in the screen coordinate system
+     * @param xc x-coordinate of the center
+     */
+    public void setXc(double xc){
+        this.xc = xc;
+    }
+
+    
+    /** 
+     * Setter method for setting the y-coordinate of the center of the shape in the screen coordinate system
+     * @param yc y-coordinate of the center
+     */
+    public void setYc(double yc){
+        this.yc = yc;
+    }
+
+    
+    /** 
+     * Setter method for setting the x-coordinates of the vertices (in counter-clockwise order) of the shape in its local coordinate system
+     * @param xLocal an array of x-coordinates of the vertices (in couter-clockwise order)
+     */
+    public void setXLocal(double[] xLocal){
+        this.xLocal = xLocal;
+    }
+
+    
+    /** 
+     * Setter method for setting the y-coordinates of the vertices (in counter-clockwise order) of the shape in its local coordinate system
+     * @param yLocal an array of y-coordinates of the vertices (in couter-clockwise order)
+     */
+    public void setYLocal(double[] yLocal){
+        this.yLocal = yLocal;
+    }
+
+    //Other methods
+
+    /** 
+     * Translates the center of the shape by the parameters dx and dy respectively, along the x and y directions of the screen coordinate system. In other words, add parameters dx and dy respectively to instance variables xc and yc. 
+     * @param dx distance to be shifted in x direction
+     * @param dy distance to be shifted in y direction
+     */
+    public void translate(double dx, double dy){
+        this.xc += dx;
+        this.yc += dy;
+    }
+
+    
+    /** 
+     * Rotates the shape about its center by the parameter dt (in radians). In other words, add parameter dt to instance variable theta.
+     * @param dt angle to rotate (in radians)
+     */
+    public void rotate(double dt){
+        this.theta += dt;
+    }
+
+    /** 
+     * Returns the x-coordinates of the vertices (in counter-clockwise order) of the shape in the screen coordinate system.
+     * @return int[] the array of x-coordinates
+     */
+    public int[] getX(){
+        int[] xVerticesScreen = new int[this.xLocal.length]; //Allocating memory in heap
+        for(int i = 0; i < this.xLocal.length; i++) //Computes and saves in the new array
+            xVerticesScreen[i] = (int) Math.round( this.xLocal[i]*Math.cos(this.theta) - this.yLocal[i]*Math.sin(this.theta) + this.xc );
+        return xVerticesScreen;
+    }
+
+    
+    /** 
+     * Returns the y-coordinates of the vertices (in counter-clockwise order) of the shape in the screen coordinate system.
+     * @return int[] the array of y-coordinates
+     */
+    public int[] getY(){
+        int[] yVerticesScreen = new int[this.yLocal.length]; //Allocating memory in heap
+        for(int i = 0; i < this.yLocal.length; i++) //Computes and saves in the new array
+            yVerticesScreen[i] = (int) Math.round( this.xLocal[i]*Math.sin(this.theta) + this.yLocal[i]*Math.cos(this.theta) + this.yc );
+        return yVerticesScreen;
+    }
+}
